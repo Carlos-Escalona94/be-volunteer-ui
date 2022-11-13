d0bbc632284e8b61b7bf764c97f8c2a0fb8434dd
carlos.escalona <carlos.escalona@gmail.com>
2022-11-13 20:31:57 -0300

    react router config

---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles"
+
+const Contact = () => {
+    return (
+        <Container>
+            <h1>The Contact Page</h1>
+        </Container>
+    )
+}
+
+export default Contact;
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles'
+
+export function NavMenu() {
+
+  return (
+    <Container>
+    </Container>
+  )
+}
+ 
